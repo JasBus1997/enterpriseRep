b0806f231211dbf1c8a25496b9330dc73e5476fe
Jasmine Busuttil <Jasmine.Busuttil.c10261@mcast.edu.mt>
2020-11-10 13:22:27 +0100

    lesson5

---
+++ assignment/ShoppingCart.Data/repositories/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using ShoppingCart.Data.Context;
+using ShoppingCart.Domain.interfaces;
+using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart.Data.repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        ShoppingCartDbContext _context;
+        public ProductRepository(ShoppingCartDbContext context)
+        {
+            _context = context;
+        }
+
+        public Guid AddProduct(Product p)
+        {
+            _context.Products.Add(p);
+            _context.SaveChanges(); //saves changes in memory even after you switch off your server/pc
+            return p.Id;
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            _context.Products.Remove(p);
+            _context.SaveChanges();
+        }
+
+        public Product GetProduct(Guid id)
+        {
+            return _context.Products.SingleOrDefault(x => x.Id == id); // single or default will return 1 product or NULL
+        }
+
+        public IQueryable<Product> GetProducts()
+        {
+            return _context.Products;
+        }
+    }
+}
---
+++ assignment/ShoppingCart.Domain/interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart.Domain.interfaces
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> GetProducts(); //querable list / list of products - advantages; in it stores the commands
+        Product GetProduct(Guid id);
+
+        void DeleteProduct(Product p);
+
+        Guid AddProduct(Product p);
+    }
+}
