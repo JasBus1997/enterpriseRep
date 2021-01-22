f22af4c2b0c8bca290ab5a193564776f938bdaec
Jasmine Busuttil <Jasmine.Busuttil.c10261@mcast.edu.mt>
2021-01-22 16:52:10 +0100

    arranging things

--- SWD62BEP-main/Solution3/ShoppingCart.Domain/Models/Product.cs
+++ SWD62BEP-main/Solution3/ShoppingCart.Domain/Models/Product.cs
@@ -5,59 +5,30 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
-
-
-
-//Code first approach
-
-
-
 namespace ShoppingCart.Domain.Models
 {
-    //once you have updated your product with new columns >>>>>>>> ctrl + shift + b
-
-
-
     public class Product
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid Id { get; set; } // C581B912-DF44-434B-8AD4-5343FC087507
-
-
+        public Guid Id { get; set; }
 
         [Required]
         public string Name { get; set; }
         [Required]
         public double Price { get; set; }
-
-
-
         [Required]
         public string Description { get; set; }
         [Required]
-        public virtual Category Category { get; set; } //this is the relationship
-
-
+        public virtual Category Category { get; set; } 
 
         [ForeignKey("Category")]
-        public int CategoryId { get; set; } //this is the actual foreign key; this is a way how to address the relationship
-
-
-
-
+        public int CategoryId { get; set; }
         public string ImageUrl { get; set; }
-
-
-
         public int Stock { get; set; }
 
-
-
         [DefaultValue(false)]
-        public bool Disable { get; set; } //to refresh the db,you must always run Add-Migration & Update-Database
-
-
+        public bool Disable { get; set; } 
 
     }
 }
